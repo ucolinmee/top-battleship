ab21fdad2da68e12dc9c3b331f58de315b9b9522
ucolinmee <colinjmeehan23@gmail.com>
2024-08-12 10:30:27 +0800

    Place ships randomly

--- src/GameLogic.js
+++ src/GameLogic.js
@@ -1,13 +1,41 @@
 import Player from "./Player/Player";
 
-
+const randomInitialShips = (player) => {
+    let directions = ['H', 'V'];
+    let shipsToPlace = [2, 2, 3];
+    
+    for (let i = 0; i < shipsToPlace.length; i++) {
+        let successfullyPlaced = false;
+        let coordinates = [];
+        while (!successfullyPlaced) {
+            let direction = _.sample(directions); // _.sample() is from lodash library for making random choice from array
+            coordinates = [];
+            if (direction == 'H') { // Places ship horizontally, checks for collision with other already placed ships
+                let rowStart = Math.floor((Math.random() * 10));
+                let colStart = Math.floor(Math.random() * (10 - shipsToPlace[i]));
+                for (let j = 0; j < shipsToPlace[i]; j++) {
+                    if (player.board.board[rowStart][colStart + j].ship) continue
+                    coordinates.push([rowStart, colStart + j]);
+                }                
+            } else { // Places ship vertically, checks for collision with other already placed ships
+                let rowStart = Math.floor(Math.random() * (10 - shipsToPlace[i]));
+                let colStart = Math.floor((Math.random() * 10));
+                for (let j = 0; j < shipsToPlace[i]; j++) {
+                    if (player.board.board[rowStart + j][colStart].ship) continue
+                    coordinates.push([rowStart + j, colStart]);
+                }  
+            }
+            successfullyPlaced = coordinates.length == shipsToPlace[i] ? true : false;
+        }
+        console.log(coordinates);
+        player.board.placeShip(coordinates);
+    }
+}
 
 let user = new Player('user');
-user.board.placeShip([[0,0], [0,1], [0,2]]);
-user.board.placeShip([[7,9], [8,9], [9,9]]);
+randomInitialShips(user);
 
 let bot = new Player('bot');
-bot.board.placeShip([[0,0], [0,1], [0,2]]);
-bot.board.placeShip([[7,9], [8,9], [9,9]]);
+randomInitialShips(bot);
 
 export {user, bot}
